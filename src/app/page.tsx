71ea32ed25a3a6c9e1e955f77f836703b19d61f0
Isabelle <isa_landini@hotmail.com>
2026-08-12 18:39:49 -0300

    feat: cria listagem inicial de artigos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,32 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+type Artigo = {
+  _id: string,
+  titulo: string,
+  autor: string,
+  data: string,
+  conteudo: string,
+  slug: string
+}
 
-export default function Home() {
+async function Home () {
+  const response = await fetch('https://crudcrud.com/api/608c972c7415451fa842b3d80d68add6/artigos')
+  
+  const artigos: Artigo[] = await response.json()
+  
+  console.log( artigos )
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.tsx</code> file.
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main>
+      <h1>Blog</h1>
+      {artigos.map((artigo) =>
+        <article key = {artigo._id}>
+            <h2>{artigo.titulo}</h2>
+            <p>{artigo.autor}</p>
+            <p>{artigo.data}</p>
+            <p>{artigo.conteudo}</p>
+          </article>
+        )}
+    </main>
   );
 }
+
+export default Home
